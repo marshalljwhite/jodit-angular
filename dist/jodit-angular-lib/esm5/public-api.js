377847c2f9324d5cc66575ce1e5826bc840e5aa0
marshalljwhite <hi@marshallwhite.ca>
2020-05-28 12:13:31 -0400

    Commit dist

---
+++ dist/jodit-angular-lib/esm5/public-api.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview added by tsickle
+ * Generated from: public-api.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/*
+ * Public API Surface of jodit-angular-lib
+ */
+export { JoditAngularComponent } from './lib/jodit-angular.component';
+export { JoditAngularModule } from './lib/jodit-angular.module';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2pvZGl0LWFuZ3VsYXIvIiwic291cmNlcyI6WyJwdWJsaWMtYXBpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBSUEsc0NBQWMsK0JBQStCLENBQUM7QUFDOUMsbUNBQWMsNEJBQTRCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIGpvZGl0LWFuZ3VsYXItbGliXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9saWIvam9kaXQtYW5ndWxhci5jb21wb25lbnQnO1xuZXhwb3J0ICogZnJvbSAnLi9saWIvam9kaXQtYW5ndWxhci5tb2R1bGUnO1xuIl19
